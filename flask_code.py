9b5882b7167884da3d754bea2d326b2b468f1e8d
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-02-17 23:00:49 +0530

    Removed __name___ == '__main__'

--- flask_code.py
+++ flask_code.py
@@ -41,7 +41,6 @@ def download(filename):
 # def download(filename):
 #     return render_template('download.html', )
 
-if __name__ == '__main__':
-    from os import environ
-    pool = multiprocessing.Pool(processes = 5)
-    app.run (port = environ.get("PORT", 5050), debug = True)
+from os import environ
+pool = multiprocessing.Pool(processes = 5)
+app.run (port = environ.get("PORT", 5050), debug = True)
