247ef2390c9f5e10abd1ae52d39b60b7cdc52857
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-02-17 22:47:58 +0530

    Main executable is flask_code.py Made many changes to javascript

--- flask_code.py
+++ flask_code.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, send_from_directory, redirect
+from flask import Flask, render_template, request, send_from_directory, redirect, jsonify
 from personality_check import check
-from multiprocessing import Queue, Process
+import os
+import multiprocessing
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,12 +11,25 @@ def hello():
 @app.route('/get_answers', methods = ["POST"])
 def get_ans():
     personality_input = request.form.get("personality_input")
-    queue = Queue()
-    p = Process(target=check, args=(personality_input,))
-    p.start()
-    p.join() # this blocks until the process terminates
-    outfile = queue.get()
-    return redirect(url_for(download_page)+"/"+outfile)
+    res = pool.apply_async(check, (personality_input,))
+    try:
+        return res.get(timeout=8), 200
+    except TimeoutError:
+        return jsonify({
+            "msg": "Timeout, your request took just too much time"
+        }), 500
+    # print("1")
+    # queue = Queue()
+    # print("2")
+    # p = Process(target=check, args=(personality_input,))
+    # print("3")
+    # p.start()
+    # print("4")
+    # p.join() # this blocks until the process terminates
+    # print("5")
+    # outfile = queue.get()
+    # print("6")
+    # print("the outfile is "+outfile)
 @app.route('/download_page/<outfile>')
 def download_page(outfile):
     return render_template('download.html', outfile_d = "po"+outfile, outfile_l = "link_"+outfile)
@@ -26,3 +40,8 @@ def download(filename):
 # @app.route('/download_page', methods=['GET', 'POST'])
 # def download(filename):
 #     return render_template('download.html', )
+
+if __name__ == '__main__':
+    from os import environ
+    pool = multiprocessing.Pool(processes = 5)
+    app.run (port = environ.get("PORT", 5050), debug = True)
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask_code import app
-
+import multiprocessing
 if __name__ == '__main__':
     from os import environ
+    pool = multiprocessing.Pool(processes = 5)
     app.run (port = environ.get("PORT", 5050), debug = True)
