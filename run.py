148c37285bd8067007708d519e8fa8c486058626
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-02-17 22:59:00 +0530

    Moved code to flask_code.py

--- run.py
+++
@@ -1,6 +0,0 @@
-from flask_code import app
-import multiprocessing
-if __name__ == '__main__':
-    from os import environ
-    pool = multiprocessing.Pool(processes = 5)
-    app.run (port = environ.get("PORT", 5050), debug = True)
